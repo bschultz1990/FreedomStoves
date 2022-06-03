061600fd30ff609d5ac247dd99f8e8b6109576b5
Ben S <bschultz1990@users.noreply.github.com>
2022-06-03 13:12:22 -0500

    finished top nav and mobile nav

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+const mobilenav = document.querySelector(".mobile-nav");
+
 const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
+    mobilenav.classList.toggle("mobile-active");
 });
+const navimage = document.querySelector("#nav-image");
+
+const navImgCheckFunction = function navImageCheck() {
+    if (window.innerWidth < 991) {
+        navimage.src = "media/sitelogo-small.png";
+    } else navimage.src = "media/sitelogo-full.png";
+};
+
+window.addEventListener("load", navImgCheckFunction);
+
+window.addEventListener("resize", navImgCheckFunction);
