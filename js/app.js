ee0ae8d5dacade33d79203e3446ee70e46d70939
Ben S <bschultz1990@users.noreply.github.com>
2022-06-07 16:50:46 -0500

    Floated the hamburger menu.

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ const navimage = document.querySelector("#nav-image");
 
 const navImgCheckFunction = function navImageCheck() {
     if (window.innerWidth < 991) {
-        navimage.src = "media/sitelogo-small.png";
-    } else navimage.src = "media/sitelogo-full.png";
+        navimage.src = "/media/sitelogo-small.png";
+    } else navimage.src = "/media/sitelogo-full.png";
 };
 
 window.addEventListener("load", navImgCheckFunction);
