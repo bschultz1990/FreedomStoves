7dc43d783153fa98bb07419f5f67f91a633dfb27
Ben Schultz <ben@schultz.com>
2022-08-03 11:18:07 -0500

    Added files via CMD

--- scratchpad.js
+++ scratchpad.js
@@ -1,2 +1,2 @@
-// Display door width.
-door.addEventListener("resize", function () {return console.dir(door.outerWidth)})
+// Display window width.
+window.addEventListener("resize", function () {return console.dir(window.outerWidth)})
